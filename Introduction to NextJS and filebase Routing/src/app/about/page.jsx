72a499719a67ae4ee491463e818a17c7c2952000
Asad <asadalipuh5@gmail.com>
2025-02-18 11:42:07 +0500

    Introduction to NextJS

---
+++ Introduction to NextJS and filebase Routing/src/app/about/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <h1 className="text-xl">Problems solved by NextJS</h1>
+        <ul>
+            <li>Full Stack Solution</li>
+            <li>Provides File based Routing</li>
+            <li>Additional Features like Next Router</li>
+            <li>Optimized rendering</li>
+        </ul>
+    </div>
+  )
+}
+
+export default page
---
+++ Introduction to NextJS and filebase Routing/src/app/contact/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>I am a contact</div>
+  )
+}
+
+export default page
